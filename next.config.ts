b1756ce02dfacf83ca3a72f715daaf5434d17f0f
Al Imran <imranmmcbd@users.noreply.github.com>
2026-03-31 02:00:23 +0600

    Update next.config.ts
    For broken image from backend

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'backend.ibadateshop.com',
+        port: '',
+        pathname: '/storage/**',
+      },
+    ],
+  },
+  // Keep your Turbopack settings
   turbopack: {
     root: __dirname,
   },
